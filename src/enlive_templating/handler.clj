110a0eef305666b13c80bfb7f4e38a1c17a2c4ba
SATO Yozo <yoozoosato@gmail.com>
2013-11-16 15:16:03 +0900

    Show random-number inside plain html template.
    ここのページのガイドが非常に参考になった。
    http://qiita.com/sirohuku/items/433a325e02ee8aa1adfd

--- src/enlive_templating/handler.clj
+++ src/enlive_templating/handler.clj
@@ -5,8 +5,14 @@
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/random-number" [] (random-number))
   (route/resources "/")
   (route/not-found "Not Found"))
+
+(html/deftemplate random-number {:parser html/xml-parser}
+  "templates/random-number.html"
+  []
+  [:div] (html/substitute (str (rand-int 10))))
 
 (def app
   (handler/site app-routes))
