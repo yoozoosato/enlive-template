3c84daf56c4108a7e7548666db7aaf6d6b654397
SATO Yozo <yoozoosato@gmail.com>
2013-11-16 11:50:41 +0900

    initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to enlive-templating
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/enlive_templating/core_test.clj
@@ -0,0 +1,7 @@
+(ns enlive-templating.core-test
+  (:require [clojure.test :refer :all]
+            [enlive-templating.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
