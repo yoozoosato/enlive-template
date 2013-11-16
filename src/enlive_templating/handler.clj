431b725ea7f15f24b71c73e17de6a5621b787c66
SATO Yozo <yoozoosato@gmail.com>
2013-11-16 15:47:19 +0900

    Add sign-up form.

--- src/enlive_templating/handler.clj
+++ src/enlive_templating/handler.clj
@@ -4,16 +4,23 @@
             [compojure.route :as route]
             [net.cgrand.enlive-html :as html]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (GET "/random-number" [] (random-number))
-  (route/resources "/")
-  (route/not-found "Not Found"))
-
 (html/deftemplate random-number {:parser html/xml-parser}
   "templates/random-number.html"
   []
   [:div] (html/substitute (str (rand-int 10))))
+
+(html/deftemplate signup {:parser html/xml-parser}
+  "templates/signup.html"
+  []
+  [:div#pg_1] (html/substitute "Please sign up.")
+)
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (GET "/random-number" [] (random-number))
+  (GET "/signup" [] (signup))
+  (route/resources "/")
+  (route/not-found "Not Found"))
 
 (def app
   (handler/site app-routes))
