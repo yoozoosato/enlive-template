2cf3b95cf2bb73b1112051e16c481153ca477b72
SATO Yozo <yoozoosato@gmail.com>
2013-11-16 14:58:16 +0900

    Change project template to 'compojure' template.
    Since the project was scaffold with default templates, copied some files
    from newly created one.

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [enlive "1.1.1"]])
+                 [enlive "1.1.1"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-ring "0.8.8"]]
+  :ring {:handler enlive-templating.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
---
+++ test/enlive_templating/test/handler.clj
@@ -0,0 +1,14 @@
+(ns test-compojure.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        test-compojure.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
